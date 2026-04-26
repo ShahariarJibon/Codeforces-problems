2c5eeba8133a200d96fb3bea1b977280ea6cb54a
ShahariarJibon <shahariarhossain674@gmail.com>
2026-04-26 23:39:52 +0600

    Add 1716A: 2-3 Moves

---
+++ 1716/1716A - 2-3 Moves/1716A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        int ans;
+        if(n==1) ans=2;
+        else{
+            ans=(n+2)/3;
+        }
+
+        cout<<ans<<endl;
+
+    }
+    return 0;
+}
