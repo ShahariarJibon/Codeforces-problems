550d3439e9193b58d1ab0304bc06152d8e3c0b80
ShahariarJibon <shahariarhossain674@gmail.com>
2026-04-26 23:41:34 +0600

    Add 1777A: Everybody Likes Good Arrays!

---
+++ 1777/1777A - Everybody Likes Good Arrays_/1777A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        long long a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int c = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (a[i] % 2 == a[i + 1] % 2)
+            {
+                c++;
+            }
+        }
+        cout << c << endl;
+    }
+    return 0;
+}
