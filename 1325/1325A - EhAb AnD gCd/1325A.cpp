c377ddd17fe30b37d5960b228642b75059bb9981
ShahariarJibon <shahariarhossain674@gmail.com>
2026-04-26 23:44:58 +0600

    Add 1325A: EhAb AnD gCd

---
+++ 1325/1325A - EhAb AnD gCd/1325A.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+long long gcd(long long a,long long b)
+{
+    while (b != 0)
+    {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+
+long long lcm(long long a,long long b)
+{
+    if (a == 0 || b == 0)
+        return 0;
+    return a / gcd(a, b) * b;
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        long long x;
+        scanf("%lld", &x);
+        long long a = 1;
+        long long b = x - 1;
+        printf("%lld %lld\n", a, b);
+    }
+
+    
+    return 0;
+}
