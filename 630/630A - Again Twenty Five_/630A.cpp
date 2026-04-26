e65ba858c18786ad389c03a2a4a89188242f3824
ShahariarJibon <shahariarhossain674@gmail.com>
2026-04-26 23:56:07 +0600

    Add 630A: Again Twenty Five!

---
+++ 630/630A - Again Twenty Five_/630A.cpp
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   long long int n,i,multi=1;
+   scanf("%lld",&n);
+   if(n==1)
+   printf("5");
+   if(n>1)
+   printf("25");
+    return 0;
+}
