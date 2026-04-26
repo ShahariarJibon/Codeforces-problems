1424c50370a63ceb54af5208648d4f619c6dcebb
ShahariarJibon <shahariarhossain674@gmail.com>
2026-04-26 23:43:46 +0600

    Add 1783A: Make it Beautiful

---
+++ 1783/1783A - Make it Beautiful/1783A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        if (a[0] == a[n - 1])
+        {
+            cout << "NO\n";
+            continue;
+        }
+
+        cout << "YES\n";
+
+        int last = a[n - 1];
+        for (int i = n - 1; i > 0; i--)
+        {
+            a[i] = a[i - 1];
+        }
+        a[0] = last;
+
+        for (int i = 0; i < n; i++)
+            cout << a[i] << " ";
+        cout << "\n";
+    }
+    return 0;
+}
